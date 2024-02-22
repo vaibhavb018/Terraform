2e0c73927495372ccbcf4252bbc508b0f86270d3
vaibhavb018 <vrbiradaar15@gmail.com>
2024-02-22 22:29:53 +0530

    --- Auto Git Commit ---

--- day1/eks.tf
+++ day1/eks.tf
@@ -1,51 +1,43 @@
 provider "aws" {
- region = "ap-southeast-1"
+  region = "ap-southeast-1"
 }
 
-data "aws_iam_policy_document" "iam_role_data" {
-  statement {
-    effect = "Allow"
+resource "aws_eks_cluster" "my_cluster" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster.arn
+  version  = "1.28"
 
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-#Iam Role
-
-resource "aws_iam_role" "v-role" {
-  name               = "v-role"
-  assume_role_policy = data.aws_iam_policy_document.iam_role_data.json
-}
-
-
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.iamrole.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.iamrole.name
-}
-
-resource "aws_eks_cluster" "cluster" {
-  name     = "my-cluster"
-  role_arn = aws_iam_role.iamrole.arn
   vpc_config {
-    subnet_ids = [
-      "subnet-080137d5f3f665b19",
-      "subnet-090f17d7ae9f58be2",
-      "subnet-04c181f0a61d345cc"
-    ]
+    subnet_ids = ["subnet-080137d5f3f665b19", "subnet-090f17d7ae9f58be2"]
   }
+}
 
-depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
-     aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
   ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_service_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster.name
 }

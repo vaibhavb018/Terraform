91659e5920ca72f85c2215ad9621baccba0791d3
vaibhavb018 <vrbiradaar15@gmail.com>
2024-02-23 08:22:14 +0530

    updated

--- day1/eks.tf
+++ day1/eks.tf
@@ -5,7 +5,6 @@ provider "aws" {
 resource "aws_eks_cluster" "my_cluster" {
   name     = "my-eks-cluster"
   role_arn = aws_iam_role.eks_cluster.arn
-  version  = "1.28"
 
   vpc_config {
     subnet_ids = ["subnet-080137d5f3f665b19", "subnet-090f17d7ae9f58be2"]
